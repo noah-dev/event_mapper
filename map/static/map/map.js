98b9b1227fec372047b2ca964a49d552aa976f1c
Noah-Dev <ark_code@protonmail.com>
2017-08-20 23:17:26 -0500

    Preparing for Heroku Deployment

--- map/static/map/map.js
+++ map/static/map/map.js
@@ -2,20 +2,14 @@ var map
 var zoom = 11
 var kcmo = {lat: 39.1, lng: -94.6};
 var meetups 
+var meetups_loaded = 0
 
 $(function() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: zoom,
         center: kcmo
     });
-
-    populate_map()
-
-    /*
-    google.maps.event.addListenerOnce(map, 'idle', function(){
-        populate_map()
-    });
-    */
+    get_data()
 });
 
 function add_marker(title, desc, lat, lng){
@@ -30,8 +24,31 @@ function add_marker(title, desc, lat, lng){
     marker.addListener('click', function(){
         infowindow.open(map, marker)
     });
+};
+
+function get_data(){
+    $.ajax({
+        url: 'meetups_data/',
+        data: {
+        },
+        dataType: 'json',
+        success: function (data) {
+            meetups = data; 
+        }
+    });
 }
 
+$( "#load_5" ).click(function() {
+    if (meetups.length != meetups_loaded){
+        for (i = meetups_loaded; i < meetups_loaded+5; i++) {
+            meetup = meetups[i]
+            add_marker(meetup['title'], meetup['desc'] + " | " + meetup['date'], meetup['lat'], meetup['lng'])
+        } 
+        meetups_loaded += 5
+    }
+});
+
+/*
 function populate_map(){
 
     //Setup the ajax request and send it off - handled by urls.py
@@ -53,3 +70,4 @@ function populate_map(){
         }
     });
 }
+*/
