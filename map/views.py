66cf2a4babbddd1c43cf1c2214f4effbb2bd7b98
Noah-Dev <ark_code@protonmail.com>
2017-09-11 13:35:31 -0500

    Still strange

--- map/views.py
+++ map/views.py
@@ -12,12 +12,9 @@ def privacy(request):
     return render(request, 'map/privacy.html')
 
 def meetups_data(request):
-    print(timezone.now())
     key = os.environ.get("MEETUP_API_KEY")
     meetup_api_request = "https://api.meetup.com/find/events?key=" + key +"&photo-host=public&sig_id=229046722&radius=10.0&lon=-94.6275&lat=39.1141"
-    print(meetup_api_request)
     meetups = json.loads(requests.get(meetup_api_request).text)
-    print(timezone.now())
     
     meetups_data = []
     for meetup in meetups:
@@ -57,7 +54,6 @@ def meetups_data(request):
         else:
             pass
     
-    print(timezone.now())
     return JsonResponse(meetups_data, safe=False)
 
 # -------------------------------------
