c875c0abcd620a454872db2868afb7f34f516d17
Noah-Dev <ark_code@protonmail.com>
2017-09-11 17:43:38 -0500

    Initial angular refactoring complete.

--- map/static/map/app.js
+++ map/static/map/app.js
@@ -17,26 +17,40 @@ app.controller('list', function($scope, $http) {
         from_unix = moment($scope.end['_d']).unix();
     };
 
+    //Local variable 
+    var local_map;
+    var local_markers = [];
+    var local_infowindow;
+    initMap();
 
     $scope.populate = function(){
-        populate_map_and_list()
+        populate_map_and_list(local_markers, local_infowindow, local_map, to_unix, from_unix)
     }
+    
+    
+    $scope.$watch('visible_events', function() {
+        if (typeof local_map != 'undefined'){
+            show_filtered_meetups(local_markers, local_map);
+        };
+    });
+    
 
-    var local_map 
-    var local_makers = []
-    initMap(local_makers, local_map);
 
-    function initMap(markers, map){
+    function initMap(){
         map = new google.maps.Map(document.getElementById('map'), {
             zoom: 11,
             center: {lat: 39.1, lng: -94.6},
         });
-        var infowindow = new google.maps.InfoWindow({})
-        populate_map_and_list(markers, infowindow, map, to_unix, from_unix)
+        infowindow = new google.maps.InfoWindow({})
+
+        local_map = map;
+        local_infowindow = infowindow;
+
+        populate_map_and_list(local_markers, infowindow, map, to_unix, from_unix)
     }
 
     function reset_map(markers){
-        if (markers.length = 0){
+        if (markers.length != 0){
             for (var i = 0; i < markers.length; i++) {
                 markers[i]['marker'].setMap(null);
             }
@@ -60,15 +74,17 @@ app.controller('list', function($scope, $http) {
         });
         data_req.then(data=>{
             var events = data.data
+            $scope.events = events;
+            
             for (var i = 0; i < events.length; i++) {
                 event = events[i]
-                add_marker(markers, map, event['index'], event['title'], event['desc'], event['lat'], event['lng'])
-                if (i-1 == events.length ){show_filtered_meetups(markers, infowindow, map);}
+                add_marker(markers, infowindow, map, event['index'], event['title'], event['desc'], event['lat'], event['lng'])
+                if (i-1 == events.length ){show_filtered_meetups(markers, map);}
             }
         });
     }
 
-    function add_marker(markers, map, index, title, desc, lat, lng){
+    function add_marker(markers, infowindow, map, index, title, desc, lat, lng){
         var marker = new google.maps.Marker({
             position: {lat: lat, lng: lng},
             title: title
@@ -90,8 +106,8 @@ app.controller('list', function($scope, $http) {
     */
     function show_filtered_meetups(markers, map){
         if (reset_map(markers)){
-            for (var i = 0; i < $scope.visible_meetups.length; i++) {
-                markers[$scope.visible_meetups[i]['index']]['marker'].setMap(map);
+            for (var i = 0; i < $scope.visible_events.length; i++) {
+                markers[$scope.visible_events[i]['index']]['marker'].setMap(map);
             }
         }
     }
