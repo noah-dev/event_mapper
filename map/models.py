f8e9a1ca0c6a7d485431ab714449f1bd047a960a
Noah-Dev <ark_code@protonmail.com>
2017-09-25 09:24:17 -0500

    Updating model

--- map/models.py
+++ map/models.py
@@ -4,7 +4,7 @@ class tag_store(models.Model):
     '''Represents an item on a todo list'''
     
     key = models.CharField(max_length=200)
-    cat = models.TextField()
+    cat = models.CharField(max_length=200)
 
     def __str__(self):
         return self.key + " | " + self.cat 
